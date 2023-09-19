bf7de07fcc58aa456c7f7e9dcf2d46c3716d3fd2
Gao Rui <125622774+gaoray08@users.noreply.github.com>
2023-09-19 09:55:31 +0800

    Update homework2.py

--- homework2.py
+++ homework2.py
@@ -1,10 +1,12 @@
 '''
+q1
 listnum = []
 for i in range(2):
     word = input("Enter a word: \n")
     if len(word)  == 4:
         listnum.append(word)
-       
+
+q2
 
 numlist2 = []
 count = 0
@@ -37,7 +39,7 @@ while True:
             print("Try again")
             continue
             
-
+q3
 
 List = []
 count = 0
@@ -56,7 +58,7 @@ while True:
     else:
         break
     
-
+q4
 
 string = ""
 count = 0
@@ -75,6 +77,8 @@ while True:
     else:
         break
 
+q5
+
 
 List = []
 index = 0
@@ -84,4 +88,111 @@ while True:
     for i in sentence:
         List.append(i[index])
         index ++
+
+q6
+
+specialBox = []
+count = 0
+
+while count != 1:
+    word = input("Enter a 2 letter word")
+    if word[0].isalpha() == True:    
+        if word[0] in "ABCDEFGabcdefg":
+            if word[1].isdigit() == True:
+                if word[1] in "56789":
+                    specialBox.append(word)
+                    count ++
+                else:
+                    print("Second digit must be between 5,6,7,8,9, inclusive.")
+                    print("Try again.")
+                    continue
+            else:
+                print("second digit must be a digit.")
+                print("Try again.")
+                continue
+        else:
+            print("First word must be between A to G")
+            print("Try again.")
+            continue
+    else:
+        print("First word must be alpha.")
+        print("Try again.")
+        continue
+
+q7
+
+import random as r
+
+d1 = str(r.randint(0,9))
+d2 = str(r.randint(0,9))
+w1 = r.choice("abcdefghijklmnopqrstuvwxyzABCDEFGHIKLMNOPQRSTUVWXYZ")
+w2 = r.choice("abcdefghijklmnopqrstuvwxyzABCDEFGHIKLMNOPQRSTUVWXYZ")
+
+randomPassword = d1 + d2 + w1 + w2
+
+print(randomPassword)
+
+
+q8 
+
+
+import random as r
+randomNum = r.randint(10,39)
+count = 0
+
+while True:
+    if count == 6:
+        print("You lost!")
+        break
+    guess = int(input("Guess the random number: \n"))
+    if guess < randomNum:
+        print("Number is greater than your guess")
+        count += 1 
+        continue
+    if guess > randomNum:
+        print("Number is smaller than your guess")
+        count += 1
+        continue
+    if guess == randomNum:
+        print("Correct! Congratulations")
+        print(f"Number of tries taken: {count}")
+        break
+
+q9
+
+vowels = "aeiouAEIOU"
+index = 0
+word = input("Enter a word: \n")
+while True:
+    if index == len(word):
+        break
+    for i in word:
+        index += 1
+        if i in vowels:
+            print(f"The vowel {i} is in your word index {index}.")
+
+q10
+
+lists = []
+index = 0
+
+while len(lists) != 5:
+    word = input("Enter a word \n")
+    if word == "qqq":
+        break
+    
+    elif index > 0:
+        if len(word) > len(lists[index - 1]): 
+            lists.append(word)
+            index += 1
+        else:
+            print("Entry rejected")
+            continue
+    
+    elif index == 0:
+        lists.append(word)
+        index += 1
+        
+print(lists)
+
 '''
